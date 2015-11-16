c6bfd6400551dd5178c1da7c1a0161b4dda6702a
Joshua Warren <josh@creatuity.com>
2015-11-16 13:56:49 -0500

    Adding M2 config files so that M2 demo install will work

---
+++ public/magento2/app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_dev',
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 04 Nov 2015 02:30:24 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'ffcfcccc730f9d1d1f1a067e467e0a6e',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'dev',
+        'password' => 'dev',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
